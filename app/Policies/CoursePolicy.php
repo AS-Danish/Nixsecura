c35ebfd5d4c331b6f5424a4731aaa2794e643fb7
AS-Danish <asdanish123@gmail.com>
2025-11-03 22:43:33 +0530

    Created a Course Policy

---
+++ app/Policies/CoursePolicy.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Course;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CoursePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Course $course): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Course $course): bool
+    {
+        // Only admin users can edit courses
+        return $user->is_admin === true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Course $course): bool
+    {
+        // Only admin users can delete courses
+        return $user->is_admin === true;
+    }
+}
